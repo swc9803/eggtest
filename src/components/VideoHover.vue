f16a9b2f9bba0427e7233bfa79a03ea82394981e
swc9803 <swc9803@gmail.com>
2022-01-19 06:09:39 +0900

    메인 첫번째페이지 추가

---
+++ src/components/VideoHover.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="ov">
+    <div class="wrapper">
+      <input @click="onFocus" class="watch" type="checkbox">
+      <div class="video">
+        <video src="https://www.robmillsarchitects.com/files/land/city/RMA_Web_land_city_1.mp4"
+        loop muted autoplay playsinline></video>
+      </div>
+      <div class="text">
+        <span data-text="Watch the video"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup (props, { emit }) {
+    const onFocus = () => {
+      emit('focus')
+    }
+    return {
+      onFocus
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ov {
+  overflow-x: hidden;
+  .wrapper {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    left: 50%;
+    transform: translate(-50%);
+    overflow: hidden;
+    .video {
+      height: 80vh;
+      overflow: hidden;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 15px;
+      clip-path: circle(15px at left);
+      transition: clip-path .4s ease;
+      video {
+        position: fixed;
+        background: #c4cbde;
+        top: 50%;
+        left: 50%;
+        min-width: 80%;
+        min-height: 80%;
+        width: auto;
+        height: auto;
+        transform: translate(-50%, -50%);
+      }
+    }
+    .text {
+      position: relative;
+      font-weight: 600;
+      opacity: 1;
+      transition: opacity .3s ease .2s;
+      &::before,
+      &::after {
+        content:"";
+        position: absolute;
+        bottom: 0;
+        left: -1px;
+        right: 25px;
+        height: 0;
+      }
+      &::before {
+        box-shadow: 26px 0 0 1px #1F242D;
+        right: 25px, 100%;
+        opacity: 0;
+        transition: right .5s ease-in, opacity .1s linear;
+      }
+      &::after {
+        box-shadow: 26px 0 0 1px #ffffff;
+        clip-path: circle(15px at left);
+        transition: clip-path .4s ease;
+      }
+      > span {
+        &::before,
+        &::after {
+          content: attr(data-text);
+          padding-left: 26px;
+        }
+        &::before {
+          color: #1F242D;
+        }
+        &::after {
+          color: #ffffff;
+          clip-path: circle(15px at left);
+          transition: clip-path .4s ease;
+          position: absolute;
+          left: 0;
+        }
+      }
+    }
+    .watch {
+      width: 220px;
+      height: 40px;
+      margin: auto;
+      position: absolute;
+      left: 0;
+      right: 0;
+      border-radius: 40px;
+      outline: none;
+      z-index: 2;
+      appearance: none;
+      cursor: pointer;
+      &:focus {
+        outline: 0;
+      }
+      &:hover {
+        ~ .video {
+          clip-path: circle(70px at left);
+        }
+        ~ .text {
+          &::before {
+            right: 25px;
+            opacity: 1;
+          }
+          &::after {
+            clip-path: circle(70px at left);
+          }
+          > span {
+            &::after {
+              clip-path: circle(70px at left);
+            }
+          }
+        }
+      }
+      &:checked {
+        width: 100%;
+        height: 100%;
+        border-radius: 0;
+        ~ .video {
+          clip-path: circle(100vw at left);
+        }
+        ~ .text {
+          opacity: 0;
+          transition: opacity .3s ease;
+          &::after {
+            clip-path: circle(100vw at left);
+          }
+          > span {
+            &::after {
+              clip-path: circle(100vw at left);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,139 @@
 <template>
   <!-- 초기 페이지 -->
-  <div class="container">
+  <div id="darkback" />
+  <div class="firstmain">
+    <p>신선한 사료를 사용해 건강한 닭이 낳은 신선한 달걀을 만나보세요.</p>
+    <div class="imgs slide1">
+      <img src="@/assets/main/imgs1.png">
+      <img src="@/assets/main/imgs2.png">
+      <img src="@/assets/main/imgs1.png">
+    </div>
+    <div class="imgs slide2">
+      <img src="@/assets/main/imgs2.png">
+      <img src="@/assets/main/imgs1.png">
+      <img src="@/assets/main/imgs2.png">
+    </div>
+    <div class="imgs slide3">
+      <img src="@/assets/main/imgs1.png">
+      <img src="@/assets/main/imgs2.png">
+      <img src="@/assets/main/imgs1.png">
+    </div>
+    <div class="imgs slide4">
+      <img src="@/assets/main/imgs1.png">
+      <img src="@/assets/main/imgs2.png">
+      <img src="@/assets/main/imgs1.png">
+    </div>
+  </div>
+  <div class="secondmain">
+    <VH class="vh" @focus="focusVideo"/>
+    <h1>초기 페이지</h1>
+    <h1>초기 페이지</h1>
+    <h1>초기 페이지</h1>
     <h1>초기 페이지</h1>
   </div>
 </template>
 
 <script>
-export default {
+import VH from '@/components/VideoHover'
+import { onBeforeMount, onMounted } from 'vue'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
+export default {
+  components: {
+    VH
+  },
+  setup () {
+    const focusVideo = () => {
+      const darkback = document.getElementById('darkback')
+      darkback.style.opacity = ((darkback.style.opacity !== '1') ? '1' : '0')
+    }
+    onBeforeMount(() => {
+      window.onload = function () {
+        setTimeout(function () {
+          scrollTo(0, 0)
+        }, 0)
+      }
+    })
+    onMounted(() => {
+      gsap.to('.slide1 > img', { xPercent: -200, duration: 72, ease: 'none', repeat: -1 }).progress(0.25)
+      gsap.from('.slide2 > img', { xPercent: -200, duration: 72, ease: 'none', repeat: -1 }).progress(0.5)
+      gsap.to('.slide3 > img', { xPercent: -200, duration: 72, ease: 'none', repeat: -1 }).progress(0.5)
+      gsap.from('.slide4 > img', { xPercent: -200, duration: 72, ease: 'none', repeat: -1 }).progress(0.25)
+
+      // 텍스트
+      const showtext = gsap.timeline()
+      ScrollTrigger.create({
+        animation: showtext,
+        trigger: '.firstmain',
+        start: 'top top',
+        end: '230%',
+        scrub: 1,
+        pin: true,
+        markers: true
+      })
+      showtext.from('.firstmain > p', {
+        yPercent: -20,
+        opacity: 0
+      })
+    })
+    return {
+      focusVideo
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.firstmain {
+  width: 100%;
+  height: 100vh;
+  background: rgb(185, 185, 185);
+  overflow: hidden;
+  .imgs {
+    margin-bottom: 60px;
+    position: relative;
+    top: -35%;
+    left: -10%;
+    white-space: nowrap;
+    padding: 0px;
+    transform: rotate(20deg);
+    filter: blur(1px) grayscale(0.6);
+    img {
+      width: 150%;
+      display: inline-block;
+    }
+  }
+  p {
+    background: rgba(0, 0, 0, 0.7);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    z-index: 6;
+    text-align: center;
+    word-break: keep-all;
+  }
+}
+#darkback {
+  opacity: 0;
+  position: fixed;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.4);
+  transition: .5s ease;
+}
+@media screen and (max-width: 768px) {
+  .firstmain {
+    .imgs {
+      top: 0;
+      img {
+        width: 200%;
+      }
+    }
+  }
+}
 </style>
